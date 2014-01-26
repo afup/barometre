dec258783f8445eb20634e45fb8e8f9c3e3c6cca
Adrien Gallou <adriengallou@gmail.com>
2014-01-26 20:50:41 +0100

    ajout d'un rapport de répartition par taille d'entreprise

--- src/Afup/BarometreBundle/Controller/ReportCompanySizeController.php
+++ src/Afup/BarometreBundle/Controller/ReportCompanySizeController.php
@@ -4,19 +4,19 @@ namespace Afup\BarometreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class ReportCompanySizeController extends Controller
 {
     public function indexAction()
     {
         $query = $this->get('afup.barometre.query_builder_factory')->getResponse();
 
         $query->select('count(distinct response.id) as count');
-        $query->addSelect('response.compagnyType as companyType');
-        $query->addGroupBy('response.compagnyType');
+        $query->addSelect('response.compagnySize as companySize');
+        $query->addGroupBy('response.compagnySize');
 
         $results = $query->execute();
 
-        return $this->render('AfupBarometreBundle:Default:index.html.twig', array(
+        return $this->render('AfupBarometreBundle:Report:company_size.html.twig', array(
           'results' => $results,
         ));
     }
