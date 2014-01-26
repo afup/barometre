a78e561dc4f6b5adb3f6efc65a5436b222a27c69
Benoît Lévêque <benoit@leveque.me>
2014-01-26 14:00:49 +0100

    Remplissage des tables de liaison pour certification et spécialité

--- src/Afup/BarometreBundle/Campaign/ResponseFactory.php
+++ src/Afup/BarometreBundle/Campaign/ResponseFactory.php
@@ -3,8 +3,11 @@
 namespace Afup\BarometreBundle\Campaign;
 
 use Afup\BarometreBundle\Entity\Campaign;
+use Afup\BarometreBundle\Entity\Certification;
 use Afup\BarometreBundle\Entity\Response;
+use Afup\BarometreBundle\Entity\Speciality;
 use Afup\BarometreBundle\Enums\EnumsCollection;
+use Doctrine\ORM\EntityRepository;
 use NumberFormatter;
 
 class ResponseFactory
@@ -20,13 +23,31 @@ class ResponseFactory
     private $enums;
 
     /**
-     * @param NumberFormatter $numberFormatter
-     * @param EnumsCollection $enums
+     * @var EntityRepository
      */
-    public function __construct(NumberFormatter $numberFormatter, EnumsCollection $enums)
-    {
-        $this->numberFormatter = $numberFormatter;
-        $this->enums           = $enums;
+    private $certificationRepository;
+
+    /**
+     * @var EntityRepository
+     */
+    private $specialityRepository;
+
+    /**
+     * @param NumberFormatter  $numberFormatter
+     * @param EnumsCollection  $enums
+     * @param EntityRepository $certificationRepository
+     * @param EntityRepository $specialityRepository
+     */
+    public function __construct(
+        NumberFormatter $numberFormatter,
+        EnumsCollection $enums,
+        EntityRepository $certificationRepository,
+        EntityRepository $specialityRepository
+    ) {
+        $this->numberFormatter         = $numberFormatter;
+        $this->enums                   = $enums;
+        $this->certificationRepository = $certificationRepository;
+        $this->specialityRepository    = $specialityRepository;
     }
 
     /**
@@ -50,7 +71,6 @@ class ResponseFactory
         $response->setSalarySatisfaction(
             $this->numberFormatter->parse($data["salary_satisfaction"])
         );
-
         $response->setStatus(
             $this->enums->getEnums('status')->getIdByLabel($data["status"])
         );
@@ -82,8 +102,60 @@ class ResponseFactory
             "oui" === $data["formation_impact"]
         );
 
-        // TODO certification, spécialité
+        if ("oui" === $data['has_certification']) {
+            $this->addCertification(
+                $response,
+                explode(', ', $data['certification_list'])
+            );
+        }
+
+        if (strlen(trim($data["speciality"])) !== 0) {
+            $this->addSpeciality(
+                $response,
+                explode(', ', $data['speciality'])
+            );
+        }
 
         return $response;
+    }
+
+    /**
+     * @param Response $response
+     * @param array    $certificationList
+     */
+    protected function addCertification(Response & $response, array $certificationList)
+    {
+        foreach ($certificationList as $certification) {
+
+            $certification = $this->certificationRepository->findOneBy(
+                array(
+                    'name' => trim($certification)
+                )
+            );
+
+            if (!$certification instanceof Certification) {
+                // TODO :
+                continue;
+            }
+            $response->addCertifiation($certification);
+        }
+    }
+
+    /**
+     * @param Response $response
+     * @param array    $specialityList
+     */
+    protected function addSpeciality(Response & $response, array $specialityList)
+    {
+        foreach ($specialityList as $speciality) {
+            $speciality = $this->specialityRepository->findOneBy(array('name' => trim($speciality)));
+
+            if (!$speciality instanceof Speciality) {
+                // TODO
+                continue;
+            }
+
+            $response->addSpeciality($speciality);
+        }
     }
 }
