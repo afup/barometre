d00d404f2e7c0b998b2f75c154246051a091fb9d
Adrien Gallou <adriengallou@gmail.com>
2014-01-26 20:06:32 +0100

    passage du queryBuilderFactory dans un service

--- src/Afup/BarometreBundle/Controller/DefaultController.php
+++ src/Afup/BarometreBundle/Controller/DefaultController.php
@@ -3,17 +3,12 @@
 namespace Afup\BarometreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Afup\BarometreBundle\Form\Type\FilteringType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $connection = $this->getDoctrine()->getConnection();
-        $context = $this->get('afup.barometre.context');
-
-        $qbF = new \Afup\BarometreBundle\Filtering\QueryBuilderFactory($connection, $context);
-        $query = $qbF->getResponse();
+        $query = $this->get('afup.barometre.query_builder_factory')->getResponse();
 
         $query->select('count(distinct response.id) as count');
         $query->addSelect('response.compagnyType as companyType');
