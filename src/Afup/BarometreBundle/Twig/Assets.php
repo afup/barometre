291df6eb8654159f9d80c0f1e40e223c620a7897
Adrien Gallou <adriengallou@gmail.com>
2014-01-28 23:26:20 +0100

    nettoyage de l'extension twig

--- src/Afup/BarometreBundle/Twig/Assets.php
+++ src/Afup/BarometreBundle/Twig/Assets.php
@@ -11,29 +11,29 @@ class Assets extends \Twig_Extension
 
     public function __construct($kernelRootDir)
     {
-      $this->assetsPath = $kernelRootDir . '/../web/assets/';
+        $this->assetsPath = $kernelRootDir . '/../web/assets/';
     }
 
     public function getGlobals()
     {
-      return array(
-        'compiled_css_file' => $this->getPathForPattern('*.css', 'css'),
-        'compiled_js_file' => $this->getPathForPattern('*.js', 'js'),
-      );
+        return array(
+          'compiled_css_file' => $this->getCompiledFile('*.css', 'css'),
+          'compiled_js_file' => $this->getCompiledFile('*.js', 'js'),
+        );
     }
 
-    protected function getPathForPattern($pattern, $subDir)
+    protected function getCompiledFile($pattern, $subDir)
     {
-      $finder = new Finder();
-      $files = $finder->files()->name($pattern)->in($this->assetsPath . '/' . $subDir);
-      if (1 !== count($files)) {
-        throw new \Exception('un fichier js non trouvé ?');
-      }
-      $compiledCssFile = null;
-      foreach ($files as $file) {
-        $compiledCssFile = $file->getBasename();
-      }
-      return sprintf('/assets/%s/%s', $subDir, $compiledCssFile);
+        $finder = new Finder();
+        $files = $finder->files()->name($pattern)->in($this->assetsPath . '/' . $subDir);
+        if (count($files) > 1) {
+            throw new \Exception('There should not have more than one file in the assets dir');
+        }
+        $compiledFile = null;
+        foreach ($files as $file) {
+            $compiledFile = $file->getBasename();
+        }
+        return sprintf('/assets/%s/%s', $subDir, $compiledFile);
     }
 
     /**
