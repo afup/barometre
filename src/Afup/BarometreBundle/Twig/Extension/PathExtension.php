1e4d7b8ff793cd910aab35e8e7b06923d460c232
Benjamin Lévêque <benjamin@leveque.me>
2014-01-30 19:57:38 +0100

    remove dependecy of the request stack & use the context instead

--- src/Afup/BarometreBundle/Twig/Extension/PathExtension.php
+++ src/Afup/BarometreBundle/Twig/Extension/PathExtension.php
@@ -2,18 +2,18 @@
 
 namespace Afup\BarometreBundle\Twig\Extension;
 
-use Symfony\Component\HttpFoundation\RequestStack;
+use Afup\BarometreBundle\Filtering\Context;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PathExtension extends \Twig_Extension
 {
-    private $requestStack;
+    private $context;
 
     private $generator;
 
-    public function __construct(RequestStack $requestStack, UrlGeneratorInterface $generator)
+    public function __construct(Context $context, UrlGeneratorInterface $generator)
     {
-        $this->requestStack = $requestStack;
+        $this->context = $context;
         $this->generator    = $generator;
     }
 
@@ -27,9 +27,9 @@ class PathExtension extends \Twig_Extension
         ];
     }
 
-    public function contextPath($name, $parameters = array(), $relative = false)
+    public function contextPath($name, $parameters = [], $relative = false)
     {
-        $parameters = array_merge($parameters, $this->requestStack->getCurrentRequest()->query->all());
+        $parameters = array_merge($parameters, ['q' => $this->context->getParameters()]);
 
         return $this->generator->generate($name, $parameters, $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH);
     }
