7ae36fefb7cb00b371caf6b4986f74bccf2f2008
Olivier Mansour <olivier.mansour@m6.fr>
2014-02-01 15:55:16 +0100

    about page

---
+++ src/Afup/BarometreBundle/Controller/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Afup\BarometreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AboutController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('AfupBarometreBundle:About:index.html.twig');
+    }
+} 
