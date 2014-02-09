5dc922d02e65d6e2dbd680a595fa28cddc9e5ffa
Benjamin Lévêque <benjamin@leveque.me>
2014-02-09 20:19:44 +0100

    add Variable Salary Report

---
+++ src/Afup/Barometre/Report/VariableSalaryReport.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Afup\Barometre\Report;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+
+class VariableSalaryReport implements ReportInterface
+{
+    /**
+     * @var QueryBuilder
+     */
+    private $queryBuilder;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setQueryBuilder(QueryBuilder $queryBuilder)
+    {
+        $this->queryBuilder = $queryBuilder;
+    }
+
+    /**
+     * Process the query
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->queryBuilder
+            ->select('response.experience')
+            ->addSelect('AVG(response.grossAnnualSalary) as grossAnnualSalary')
+            ->addSelect('AVG(response.variableAnnualSalary) as variableAnnualSalary')
+            ->addSelect('COUNT(response.id) as nbResponse')
+            ->groupBy('response.experience')
+            ->execute();
+    }
+
+    /**
+     * The report name (used for url)
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'variable_salary';
+    }
+
+    /**
+     * The report label (used for title & menu)
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return "report.variable_salary.label";
+    }
+}
