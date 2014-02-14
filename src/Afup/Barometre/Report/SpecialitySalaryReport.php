36ecd496a31c5db57d577756da3e325e352a110b
Benjamin Lévêque <benjamin@leveque.me>
2014-02-14 23:07:35 +0100

    correction import certifications

--- src/Afup/Barometre/Report/SpecialitySalaryReport.php
+++ src/Afup/Barometre/Report/SpecialitySalaryReport.php
@@ -80,8 +80,12 @@ class SpecialitySalaryReport implements ReportInterface
         }
 
         foreach ($data['data'] as $experience => $line) {
-            $salary = $line[$otherFramework]['annualSalary'] / $line[$otherFramework]['nbResponse'];
-            $data['data'][$experience][$otherFramework] = $salary;
+            if ($line[$otherFramework]['nbResponse']) {
+                $salary = $line[$otherFramework]['annualSalary'] / $line[$otherFramework]['nbResponse'];
+                $data['data'][$experience][$otherFramework] = $salary;
+            } else {
+                $data['data'][$experience][$otherFramework] = null;
+            }
         }
 
         return $data;
