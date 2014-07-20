5a4c11d78be5144137251064f35b14878af6cea2
Olivier Mansour <olivier.mansour@m6.fr>
2014-07-20 10:26:56 +0200

    ajout du rapport php_version

---
+++ src/Afup/Barometre/Report/PhpVersionReport.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PHP version report
+ */
+
+namespace Afup\Barometre\Report;
+
+
+class PhpVersionReport extends AbstractReport
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $this->queryBuilder
+            ->select('response.phpVersion')
+            ->addSelect('COUNT(response.id) as nbResponse')
+            ->add('where', 'response.phpVersion is not null')
+            ->having('nbResponse >= :minResult')
+            ->setParameter(':minResult', $this->minResult)
+            ->groupBy('response.phpVersion');
+
+        $this->data = $this->queryBuilder->execute();
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'php_version';
+    }
+} 
