61ce0e06a4100b896c75861f1479fa03c68a2be9
Adrien Gallou <adriengallou@gmail.com>
2014-09-12 21:22:42 +0200

    Modification du nom des logos pour leur mise en cache
    Les logos sont maintenant nommés `logo-afup.315ebee3.png`
    et `logo-agencee.23ad63c4.png`
    
    Cela va permettre d'appliquer cette configuration apache pour
    le dossier `web/assets/logos` :
    
    ```
    ExpiresActive On
    ExpiresDefault "access plus 12 month"
    ```

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\IntlBundle\SonataIntlBundle(),
+            new Agallou\GruntHashAssetsBundle\GruntHashAssetsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
