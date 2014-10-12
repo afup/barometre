8ea3fa266d27c8090cb2144a556d874b1b7a64f1
Adrien Gallou <adriengallou@gmail.com>
2014-10-12 18:26:41 +0200

    Amélioration affichage rapport satisfaction de la rémunération
    * Ajout icone pour représenter les différentes satisfactions.
    * Les couleurs ne changent plus selon l'ordre des satisfactions

--- src/Afup/BarometreBundle/Resources/assets/js/charts.js
+++ src/Afup/BarometreBundle/Resources/assets/js/charts.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
+
     $('table.highchart')
         .bind('highchartTable.beforeRender', function (event, highChartConfig) {
+
+            if ($(this).hasClass('highchart-emoticon')) {
+                var icons = $(this).data('icons');
+
+                highChartConfig.plotOptions.series = {
+                    dataLabels: {
+                        useHTML: true,
+                        formatter : function () {
+                            var defaultDatalabel = '<b>' + this.point.name + '</b> : ' + this.percentage.toFixed(2) + '%';
+                            if (typeof icons[this.point.name] === 'undefined') {
+                                return defaultDatalabel;
+                            }
+                            var infos = icons[this.point.name];
+                            return '<i class="icon ' + infos.class + '" style="font-size: ' + infos.size + '"></i> ' + defaultDatalabel;
+                        }
+                    }
+                };
+            }
+
             highChartConfig.colors[0] = '#4C6EAF';
             var align = $(this).data('graph-xaxis-labels-align');
             if (align !== undefined) {
