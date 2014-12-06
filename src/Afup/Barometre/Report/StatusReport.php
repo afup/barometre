b16c227d6a8fd66ba2909344a1f135c0039c0927
Olivier Mansour <olivier.mansour@m6.fr>
2014-12-06 23:08:41 +0100

    ajout d'un rapport sur les status
    fixes #159

---
+++ src/Afup/Barometre/Report/StatusReport.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Afup\Barometre\Report;
+
+/*
+ * Report on status
+ */
+class StatusReport  extends AbstractReport
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $this->queryBuilder
+            ->select('response.status as status')
+            ->addSelect('COUNT(response.id) as nbResponse')
+            ->addGroupBy('response.status');
+
+        $this->data = $this->queryBuilder->execute();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'status';
+    }
+} 
