3bcf5901e8ee521e15c5bfe538bb1ed4bea14d72
Olivier Mansour <olivier.mansour@m6.fr>
2015-02-03 22:43:31 +0100

    typo

--- src/Afup/BarometreBundle/Controller/CampaignController.php
+++ src/Afup/BarometreBundle/Controller/CampaignController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class CampaignController extends Controller
 {
 
-    public function Form2015Action()
+    public function form2015Action()
     {
         return $this->render('AfupBarometreBundle:Campaign:form2015.html.twig');
     }
