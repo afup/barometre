749719027635ee8329846077f7d59da9af498220
Benoît Lévêque <benoit@leveque.me>
2015-02-04 13:23:53 +0100

    Change google form size to take to whole page. and remove agence-e logo

--- src/Afup/BarometreBundle/Controller/CampaignController.php
+++ src/Afup/BarometreBundle/Controller/CampaignController.php
@@ -3,10 +3,13 @@
 namespace Afup\BarometreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class CampaignController extends Controller
 {
-
+    /**
+     * @return Response
+     */
     public function form2015Action()
     {
         return $this->render('AfupBarometreBundle:Campaign:form2015.html.twig');
