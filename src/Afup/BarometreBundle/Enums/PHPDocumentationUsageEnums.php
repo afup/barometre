c935d89a5dccc0b1e846a000fc48bc30053dde6b
Jimmy KLEIN <webcompte@mac.com>
2019-12-27 23:21:01 +0100

    Corriger l'enum sur l'usage de la documentation PHP

--- src/Afup/BarometreBundle/Enums/PHPDocumentationUsageEnums.php
+++ src/Afup/BarometreBundle/Enums/PHPDocumentationUsageEnums.php
@@ -11,7 +11,7 @@ class PHPDocumentationUsageEnums extends AbstractEnums
     protected $choices = [
         self::IN_FRENCH => 'La documentation de php.net en français',
         self::IN_ENGLISH => 'La documentation de php.net en anglais',
-        self::FIRST_FRENCH_THEN_ENGLISH => 'La documentation de php.net en français puis en anglais si elle est incomplète/incopréhensible',
+        self::FIRST_FRENCH_THEN_ENGLISH => 'La documentation de php.net en français puis en anglais si elle est incomplète/incompréhensible',
         self::AUTRE => 'Autre (documentation intégrée dans IDE par exemple)'
     ];
 }
