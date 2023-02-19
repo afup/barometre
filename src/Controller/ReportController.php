9063b78301ec716254551ac20062b1803ed12b5b
Benoît Lévêque <benoit@leveque.me>
2023-02-19 10:51:46 +0100

    correct sub-request

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -10,9 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ReportController extends AbstractController
 {
+    #[Route('/report_detail/{reportName}', name: 'afup_barometre_report_detail')]
     public function indexAction(RequestStack $requestStack, Request $request, string $reportName): Response
     {
         $masterRequest = $requestStack->getMainRequest();
@@ -61,7 +63,7 @@ class ReportController extends AbstractController
      */
     private function getManager()
     {
-        return $this->get(ReportManager::class);
+        return $this->container->get(ReportManager::class);
     }
 
     /*
