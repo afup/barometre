8f959442e76e333234d1d8d775e4e85924d43cb0
Benoît Lévêque <benoit@leveque.me>
2024-10-15 18:38:42 +0200

    ajout report 2024

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -41,10 +41,10 @@ class MenuBuilder
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
 
         $menu->addChild(
-            'menu.result2023',
+            'menu.result2024',
             [
                 'route' => 'afup_barometre_campaign',
-                'routeParameters' => ['campaignName' => 2023],
+                'routeParameters' => ['campaignName' => 2024],
                 'routeAbsolute' => UrlGeneratorInterface::ABSOLUTE_URL,
             ]
         );
